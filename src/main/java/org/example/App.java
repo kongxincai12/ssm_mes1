bc33b49f4e018578e9d762688836fbeb1d053f0b
kongxincai12 <33597957+kongxincai12@users.noreply.github.com>
2025-12-14 17:20:12 +0800

    Add files via upload

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+        /*try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }*/
+    }
+}
